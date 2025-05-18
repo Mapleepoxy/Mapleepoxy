d34d0c82e41574c499c9509bde7c54b0a74c92b4
Mapleepoxy <mapleepoxyhost@gmail.com>
2025-05-18 09:47:19 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let slideIndex = 0;
+
+  function showSlides() {
+    const slides = document.querySelectorAll(".slide");
+    slides.forEach(slide => slide.style.display = "none");
+
+    slideIndex++;
+    if (slideIndex > slides.length) slideIndex = 1;
+
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 4000); // Change every 4 seconds
+  }
+
+  document.addEventListener("DOMContentLoaded", showSlides);
+  document.querySelectorAll(".slide") // should return 3 elements
